c8c6187659c001140a102956edcf4c7d4073d4a8
marmelo7 <74640788+marmelo7@users.noreply.github.com>
2020-12-08 22:03:47 +0900

    Add files via upload
    fin i guess

--- 2.js
+++ 2.js
@@ -7,16 +7,14 @@ const checked = (e) => {
     const self = e.target;
     const text = self.nextSibling.nextSibling.style;
     
-    if(self.style.background == "white"){
-        self.style.background = "turquoise"; 
-        text.color = "lightgray";
-        text.textDecoration = "line-through";
-        return;
-    } else if (self.style.background = "turquoise"){
+    if (self.style.background == "turquoise"){
         self.style.background = "white"; 
         text.color = "black";
         text.textDecoration = "none";
-        return;
+    } else {
+        self.style.background = "turquoise"; 
+        text.color = "lightgray";
+        text.textDecoration = "line-through";
     }
 }
 
